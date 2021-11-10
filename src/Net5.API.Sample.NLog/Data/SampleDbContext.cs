7a246a958d64bafc71abed9f53814bbd4bd01e5c
Andrej Lapajne <andrej.lapajne@gmail.com>
2021-11-10 23:55:33 +0100

    Initial data structure and model

---
+++ src/Net5.API.Sample.NLog/Data/SampleDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Net5.API.Sample.Model;
+
+namespace Net5.API.Sample.Data
+{
+    public class SampleDbContext : DbContext
+    {
+        public SampleDbContext(DbContextOptions<SampleDbContext> options) : base(options) { }
+
+        public DbSet<LogEntry> LogEntries { get; set; }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    }
+}
